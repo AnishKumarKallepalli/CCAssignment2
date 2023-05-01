3443a4f9b68ce5e4f57543a7b80cdf173a62755c
Anish Kumar <anishkumar2002.k@gmail.com>
2023-05-01 13:38:16 +0530

    Done grammar

--- anish.h
+++ anish.h
@@ -1,21 +1,3 @@
-%{
-    #include <stdio.h>   
-    #include <stdlib.h> 
-    #include "ass.h"
-    extern FILE *yyin;
-    char curr_type[100];
-%}
-//for loop for later , even arrays and functions for later
-//assuming while has only expression and not assignment in grammer
-// if and else is only there...no elseif and if needs statements in curly braces
-//syntax error 0 means semicolon missing and 1 is for bracket
-%union {char *strval; int ival; char ch; float fval;}
-%token <ival> INTEGER <strval> KEYWORD <strval> IDENTIFIER <strval> CHARACTER <ch> OPERATOR <ch> PUNCTUATOR
-%token FLOAT NL EQ SC CM OB CB OC CC IF WH SW FR ELSE CL CASE DF WHILE
-%token <ival> INTC <strval> TYPE <strval> VAR <ch> CHARC <fval> FLOATC 
-%token PL MIN MUL DIV MOD NOT LT GT AND OR FOR OS CS VOID BREAK CONTINUE RET DEQ NEQ LEQ GEQ
-
-%%
 //assignment statements for arrays must have row and column
 // not handling else-if
 // function has to return
@@ -33,12 +15,12 @@ expr_stmt : expr_stmt PL expr_stmt
      | expr_stmt MUL expr_stmt
      | expr_stmt DIV expr_stmt
      | expr_stmt MOD expr_stmt
-     | expr_stmt DEQ expr_stmt
-     | expr_stmt NEQ expr_stmt
+     | expr_stmt EQ EQ expr_stmt
+     | expr_stmt NOT EQ expr_stmt
      | expr_stmt LT expr_stmt
-     | expr_stmt LEQ expr_stmt
+     | expr_stmt LT EQ expr_stmt
      | expr_stmt GT expr_stmt
-     | expr_stmt GEQ expr_stmt
+     | expr_stmt GT EQ expr_stmt
      | expr_stmt AND expr_stmt
      | expr_stmt OR expr_stmt
      | MIN expr_stmt
@@ -69,10 +51,10 @@ identifier_list: init_stmt CM identifier_list
 
 
 init_stmt : VAR
-          | VAR OS expr_stmt CS
-          | VAR OS expr_stmt CS OS expr_stmt CS
-          | VAR OS expr_stmt CS OS CS
-          | VAR EQ expr_stmt
+          | VAR OS expr CS
+          | VAR OS expr CS OS expr CS
+          | VAR OS expr CS OS CS
+          | VAR EQ expr
           ;
 
 assign_stmt : VAR EQ expr_stmt
@@ -137,32 +119,6 @@ param_list : TYPE VAR
 func_call : VAR OB val_list CB
           ;
 
-val_list : expr_stmt 
-         | expr_stmt CM val_list
+val_list : expr 
+         | expr CM val_list
          ;
-%%
-
-
-void main(){
-    FILE *fp; int i;
-    fp=fopen("input.txt","r");
-    yyin=fp;
-    yyparse();
-    struct node* Parent = (struct node*) malloc(sizeof(struct node));
-    Parent->curr_symbols =0;
-    Parent->num_children=0;
-      Parent->parent = NULL;
-    add_symbol(Parent,"Test","1D-float",10,5);
-    print_node(Parent);
-    exit(0);
-}
-
-void yyerror(){
-    printf("Error\n");
-    exit(0);
-}
-
-void syntax_error(int line){
-    printf("Syntax Error on line : %d\n",line);
-    exit(0);
-}
